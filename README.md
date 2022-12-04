eff4ddd751ca5e9360a12ba0c865810c7079a2d7
davidtazy <david.allemant@gmail.com>
2022-12-04 11:06:29 +0000

    create script to  prepare new day

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Advent of code 2022
 
 (https://adventofcode.com/2022/)[https://adventofcode.com/2022/]
+
+# create new files for a new day
+
+    > python -m template <day>
--- template/__main__.py
+++ template/__main__.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from distutils.dir_util import copy_tree
-
+import shutil
 
 # get day
 print(sys.argv[1])
@@ -19,5 +19,47 @@ assert os.path.exists(source)
 destination = f"aoc/{day_str}"
 assert not os.path.exists(destination)
 
-#copy_tree(from_directory, to_directory)
 # copy day to module
+copy_tree(source, destination)
+
+
+def inject_day(file, day_str):
+    assert os.path.exists(destination)
+    content = ""
+    with open(file, "r") as f:
+        content = f.read()
+
+    content = content.replace("dayXX", day_str)
+
+    with open(file, "w") as f:
+        f.write(content)
+
+
+inject_day(os.path.join(destination, "__main__.py"), day_str)
+
+# copy unit test
+
+source = "template/template_test_day.py"
+assert os.path.exists(source)
+
+destination = f"test/test_{day_str}.py"
+assert not os.path.exists(destination)
+
+shutil.copyfile(source, destination)
+inject_day(destination, day_str)
+
+
+# create data file to receive exemple and dataset
+
+with open(f"dataset/{day_str}.bin", 'w') as f:
+    pass
+
+with open(f"test/data/{day_str}.bin", 'w') as f:
+    pass
+
+# append test execution
+
+with open("result.sh", "a+") as f:
+    f.write(
+        f"python -m aoc.{day_str}\n"
+    )
